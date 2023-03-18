8d9c4247a4fc07e3b11fe94223a5d94e57411ec5
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-03-18 10:13:40 +0530

    Time: 78 ms (15.12%), Space: 10.6 MB (82.02%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    int solve(int n , int  m,string&x, string&y,int dp[][1001] )
-    {
-        if(n==0 || m==0 )
-            return 0 ; 
-        if(dp[n][m]!=-1)
-            return dp[n][m]; 
-        if(x[n-1]==y[m-1])
-            return dp[n][m]= 1+solve(n-1,m-1,x,y,dp); 
-        else
-            return dp[n][m]= max(solve(n-1,m,x,y,dp),solve(n,m-1,x,y,dp)); 
-    }
+    /// --------------------------------- memoization------------------------------------------//
+    // int solve(int n , int  m,string&x, string&y,int dp[][1001] )
+    // {
+    //     if(n==0 || m==0 )
+    //         return 0 ; 
+    //     if(dp[n][m]!=-1)
+    //         return dp[n][m]; 
+    //     if(x[n-1]==y[m-1])
+    //         return dp[n][m]= 1+solve(n-1,m-1,x,y,dp); 
+    //     else
+    //         return dp[n][m]= max(solve(n-1,m,x,y,dp),solve(n,m-1,x,y,dp)); 
+    // }
     int longestCommonSubsequence(string x, string y) {
       
     int n = x.size(), m = y.size(); 
@@ -20,6 +21,21 @@ public:
                 dp[i][j]=-1;
             }
         }
-        return solve(n, m , x, y ,dp); 
+        // return solve(n, m , x, y ,dp); 
+        for(int i=0 ; i<=n;i++)
+        {
+            for(int j=0 ; j<=m;j++)
+            {
+                if(i==0 || j==0)
+                    dp[i][j] = 0 ; 
+                else if(x[i-1] == y[j-1])
+                    dp[i][j] = 1+dp[i-1][j-1]; 
+                else
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]); 
+                    
+            }
+        }
+        return dp[n][m]; 
+        
     }
 };
