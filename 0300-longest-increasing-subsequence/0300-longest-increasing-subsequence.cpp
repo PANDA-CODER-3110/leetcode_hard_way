8f062c6aba3520e40aa873dc3ee905d4b5b1e4a1
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-03-18 09:46:00 +0530

    Time: 4 ms (97.74%), Space: 10.5 MB (78.40%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int>ans; 
+        for(auto x : nums)
+        {
+            if(ans.empty() || ans[ans.size()-1]<x)
+                ans.push_back(x); 
+            else
+            {
+                auto it = lower_bound(ans.begin(),ans.end(),x); 
+                *it = x ; 
+            }
+        }
+        
+        return ans.size() ; 
+            
+    }
+};
