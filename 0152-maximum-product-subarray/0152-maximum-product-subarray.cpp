fc5ba5e6d0cd32a9ba64b4b68e9b8988708129c0
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-03-18 09:28:39 +0530

    Time: 7 ms (76.16%), Space: 13.8 MB (87.30%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,33 +1,48 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-    int prod =1 ; 
-        bool zero = false ; 
-        int lef = INT_MIN,rig= INT_MIN; 
-        for(int i=0 ; i<nums.size();i++)
+      //  ------------------------------------------- O(2N)---------------------------------------------------//
+    // int prod =1 ; 
+    //     bool zero = false ; 
+    //     int lef = INT_MIN,rig= INT_MIN; 
+    //     for(int i=0 ; i<nums.size();i++)
+    //     {
+    //         if(nums[i]==0)
+    //         { prod =1 ;
+    //          zero = true ; 
+    //          continue ; }
+    //         prod *= nums[i]; 
+    //         lef = max(lef,max(prod,nums[i]));
+    //     }
+    //     prod =1 ; 
+    //     for(int j= nums.size()-1 ; j>=0 ; j--)
+    //     {
+    //         if(nums[j]==0)
+    //         {
+    //             prod =1 ;
+    //             zero = true ; 
+    //             continue ; 
+    //         }
+    //         prod*=nums[j]; 
+    //         rig = max(rig,max(prod,nums[j])); 
+    //     }
+    //     if(zero == true )
+    //     return max(lef,max(rig,0)); 
+    //     else
+    //         return max(lef,rig); 
+        
+        int ma = nums[0]; 
+        int mi = nums[0]; 
+        int ans = nums[0]; 
+        for(int i=1 ;i<nums.size();i++)
         {
-            if(nums[i]==0)
-            { prod =1 ;
-             zero = true ; 
-             continue ; }
-            prod *= nums[i]; 
-            lef = max(lef,max(prod,nums[i]));
+            if(nums[i]<0)
+                swap(ma,mi); 
+         ma=max(nums[i],nums[i]*ma);
+            mi=min(nums[i],nums[i]*mi);
+            ans = max(ans , ma ); 
         }
-        prod =1 ; 
-        for(int j= nums.size()-1 ; j>=0 ; j--)
-        {
-            if(nums[j]==0)
-            {
-                prod =1 ;
-                zero = true ; 
-                continue ; 
-            }
-            prod*=nums[j]; 
-            rig = max(rig,max(prod,nums[j])); 
-        }
-        if(zero == true )
-        return max(lef,max(rig,0)); 
-        else
-            return max(lef,rig); 
+        
+        return ans ; 
     }
 };
