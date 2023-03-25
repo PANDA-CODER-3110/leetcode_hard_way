902623554068f5902995383ba8e6de2f4218f528
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-03-25 23:56:56 +0530

    Time: 6 ms (20.50%), Space: 8.6 MB (16.19%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -12,31 +12,73 @@
 class Solution {
 public:
     // morris traversal 
-    vector<int> inorderTraversal(TreeNode* root) {
-        vector<int>ans; 
-        TreeNode* curr = root ;
-        while(curr!=NULL)
-        {
-        if(curr->left==NULL) {ans.push_back(curr->val); 
-        curr= curr->right; }
-        else
-        {
-            TreeNode* prev = curr->left; 
-            while(prev->right!=NULL  && prev->right!=curr) prev= prev->right;
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         vector<int>ans; 
+//         TreeNode* curr = root ;
+//         while(curr!=NULL)
+//         {
+//         if(curr->left==NULL) {ans.push_back(curr->val); 
+//         curr= curr->right; }
+//         else
+//         {
+//             TreeNode* prev = curr->left; 
+//             while(prev->right!=NULL  && prev->right!=curr) prev= prev->right;
             
-            if(prev->right==NULL)
-            {
-                prev->right = curr; 
-                curr= curr->left; 
-            }
-            else
-            {
-                prev->right = NULL ;
-                ans.push_back(curr->val); 
-                curr= curr->right; 
-            }
-        }
-        }
-        return ans  ;
-    }
+//             if(prev->right==NULL)
+//             {
+//                 prev->right = curr; 
+//                 curr= curr->left; 
+//             }
+//             else
+//             {
+//                 prev->right = NULL ;
+//                 ans.push_back(curr->val); 
+//                 curr= curr->right; 
+//             }
+//         }
+//         }
+//         return ans  ;
+//     }
+    
+// ALL TRVAERSAL IN ONE GO --------------------->>> EASY PEASY LEMON SQUEEEZY..................
+     vector<int> inorderTraversal(TreeNode* root)
+     {
+         stack<pair<TreeNode*, int>>st; 
+         st.push({root,1});
+             vector<int>pre,post,in; 
+         if(root ==NULL) return in; 
+         while(!st.empty())
+         {
+             auto it = st.top(); 
+             st.pop(); 
+            auto  curr = it.first ; 
+             int l = it.second ; 
+             if(l==1)
+             {
+                 pre.push_back(curr->val); 
+                 it.second++; 
+                 st.push(it); 
+                 if(curr->left!=NULL)
+                     st.push({curr->left,1}); 
+             }
+             
+             else if(l==2)
+             {
+                 in.push_back(curr->val); 
+                 it.second++; 
+                 st.push(it); 
+                 if(curr->right)
+                     st.push({curr->right,1}); 
+             }
+             
+             else 
+             {
+                 post.push_back(curr->val); 
+             }
+         }
+         
+         return in ; 
+     }
+        
+
 };
