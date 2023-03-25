f5e5f34f350e6c39fea46d64c30cf93c0e141f11
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-03-25 18:42:56 +0530

    Time: 5 ms (43.68%), Space: 12.2 MB (28.54%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,17 +11,41 @@
  */
 class Solution {
 public:
-    void solve(TreeNode* root , int level , vector<int>&ans)
-    {
-        if(root==NULL)  return ; 
-        if(level == ans.size()) ans.push_back(root->val); 
-        if(root->right) solve(root->right,level+1,ans); 
-        if(root->left)  solve(root->left, level+1,ans); 
-    }
+    // void solve(TreeNode* root , int level , vector<int>&ans)
+    // {
+    //     if(root==NULL)  return ; 
+    //     if(level == ans.size()) ans.push_back(root->val); 
+    //     if(root->right) solve(root->right,level+1,ans); 
+    //     if(root->left)  solve(root->left, level+1,ans); 
+    // }
+    
     vector<int> rightSideView(TreeNode* root) {
         vector<int>ans ; 
-        int level = 0 ; 
-        solve(root , level , ans ); 
+        if(root == NULL) return ans ; 
+        // int level = 0 ; 
+        // solve(root , level , ans ); 
+        // return ans ; 
+        queue<TreeNode*>q ;
+        q.push(root); 
+        while(!q.empty())
+        {
+            int n = q.size(); 
+            for(int i=0 ; i<n;i++)
+            {
+                auto it  = q.front(); 
+                q.pop(); 
+                if(i==n-1)
+                {
+                    ans.push_back(it->val); 
+                }
+                if(it->left)
+                    q.push(it->left); 
+                if(it->right)
+                    q.push(it->right); 
+            }
+        }
+        
         return ans ; 
+        
     }
 };
