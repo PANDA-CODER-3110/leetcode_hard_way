7a14c50d3bc3553f509a09c8ce88cb46c473c39e
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-03-26 11:57:17 +0530

    Time: 26 ms (11.06%), Space: 20.9 MB (60.30%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -9,19 +9,27 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
-int solve(TreeNode* root)
-{
-    if(root==NULL) return 0 ;
-    return 1+max(solve(root->left),solve(root->right));
-}
     bool isBalanced(TreeNode* root) {
-        if(root == NULL ) return true; 
-        
-        auto lh = solve(root->left); 
-        auto rh = solve(root->right); 
-       return abs(lh-rh)<=1 && isBalanced(root->left) && isBalanced(root->right);   
-        
+        // If the tree is empty, we can say it’s balanced...
+        if (root == NULL)  return true;
+        // Height Function will return -1, when it’s an unbalanced tree...
+		if (Height(root) == -1)  return false;
+		return true;
+	}
+    // Create a function to return the “height” of a current subtree using recursion...
+	int Height(TreeNode* root) {
+        // Base case...
+		if (root == NULL)  return 0;
+        // Height of left subtree...
+		int leftHeight = Height(root->left);
+        // Height of height subtree...
+		int rightHight = Height(root->right);
+        // In case of left subtree or right subtree unbalanced or their heights differ by more than ‘1’, return -1...
+		if (leftHeight == -1 || rightHight == -1 || abs(leftHeight - rightHight) > 1)  return -1;
+        // Otherwise, return the height of this subtree as max(leftHeight, rightHight) + 1...
+		return max(leftHeight, rightHight) + 1;
     }
 };
