a81cc8b2b80fafab7d3e754f8c90767ce4b27339
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-03-31 15:06:34 +0530

    Time: 24 ms (38.90%), Space: 17.2 MB (44.03%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -19,11 +19,31 @@ public:
 class Solution {
 public:
     Node* connect(Node* root) {
-        if(root ==NULL) return root ; 
-        if(root->left!=NULL ) root->left->next= root->right; 
-        if(root->right!= NULL && root->next!=NULL) root->right->next=root->next->left; 
-        connect(root->left); 
-        connect(root->right); 
+        // if(root ==NULL) return root ; 
+        // if(root->left!=NULL ) root->left->next= root->right; 
+        // if(root->right!= NULL && root->next!=NULL) root->right->next=root->next->left; 
+        // connect(root->left); 
+        // connect(root->right); 
+        // return root ; 
+        
+        
+        if(root==NULL)  return nullptr  ; 
+        queue<Node*>q; 
+        q.push(root); 
+        while(q.size())
+        {
+            int s = q.size();
+            Node* rightNode = nullptr;
+            for(int i = s ; i;i--)
+            {
+                 auto cur = q.front(); q.pop(); 
+                 cur->next = rightNode;   
+                rightNode = cur ; 
+               if(cur->right) q.push(cur->right),
+               q.push(cur->left); 
+            }
+        }
+        
         return root ; 
-    }
+     }
 };
