7dd6956097da3d04cf2b90e3a9c127d25d810348
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-04-01 12:15:07 +0530

    Time: 14 ms (93.53%), Space: 19.6 MB (38.32%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,7 +1,28 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
- sort(nums.begin(),nums.end()); 
-        return nums[nums.size()/2]; 
+ // sort(nums.begin(),nums.end()); 
+ //        return nums[nums.size()/2]; 
+        
+        int count = 1 , mm = nums[0]; 
+        for(int i= 1; i<nums.size();i++)
+        {
+            if(count ==0 )
+            {
+                count++;
+                 mm = nums[i]; 
+            }
+            else if(nums[i]==mm)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+        }
+        
+        
+        return mm ; 
     }
 };
