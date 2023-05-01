48d93de99b3d77e6ed5a1ee282755ca51ba6ab8a
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-05-01 18:45:28 +0530

    Time: 605 ms (25.02%), Space: 49.7 MB (63.37%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& arr) {
-        if(arr.size()<=0) return 0 ; 
-   int n = arr.size(); 
-map<int,int>mp ; 
-        for(int i=0 ; i<n;i++)
-        {
-           mp[arr[i]]=1; 
-        }
-        
-        for(auto it : arr)
-        {
-            if(mp.find(it-1)!=mp.end())
-                mp[it] = 0 ;
-        }
-        
-        int cnt =1 ;
-        int ma = 1 ; 
-        for(int i=0 ; i<n;i++)
-        {
-            if(mp[arr[i]]==1)
-            {
-                cnt =1 ; 
-                int l = arr[i]; 
-                while(mp.find(++l)!=mp.end())
-                {
-                    cnt++, ma = max(ma,cnt );
-                }
-            }
-        }
-        
-        
-        return ma  ; 
+       int N = arr.size() ; 
+        unordered_set<int>s;
+      int ans=0;
+      for(int i=0;i<N;i++)
+      {
+          s.insert(arr[i]);
+      }
+      for(int i=0;i<N;i++)
+      {
+          if(s.find(arr[i]-1)==s.end())
+          {
+              int j= arr[i];
+              while(s.find(j)!=s.end())
+              {
+                  j++;
+              }
+              
+              ans= max(ans,j-arr[i]);
+          }
+      }
+      return ans;
     }
 };
