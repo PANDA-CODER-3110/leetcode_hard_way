e2e81d286373e1d8df2247334135b348cd5f208e
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-06-01 01:33:12 +0530

    Time: 3 ms (88.71%), Space: 9.6 MB (92.68%) - LeetHub

---
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int l =0 , r = arr.size()-1 ; 
+        while(l<=r)
+        {
+            int m = l+ (r-l)/2 ; 
+            if(arr[m] - m - 1 >= k)
+                r = m-1 ; 
+            else 
+                l = m+1 ; 
+        }
+        return l+k ; 
+    }
+};
+  
+       
