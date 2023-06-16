6a54a4ad10dd9eaa761f429bc05be899b1dc199c
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-06-17 00:56:08 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (23.81%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    
+     int f (int a , int b , int i , int j ,vector<vector<int>>&dp )
+    {
+        if(i>=a+1 ||  j>=b+1)
+        return  0 ; 
+        if(i==a && j==b) return 1 ; 
+        
+        if(dp[i][j]!=-1)
+        return dp[i][j] ;
+         
+         return dp[i][j]  = f(a,b,i+1,j,dp)+f(a,b,i,j+1,dp) ; 
+         
+        
+    }
+    int uniquePaths(int m, int n) {
+        
+          vector<vector<int>>dp(m,vector<int>(n,-1));
+        return f(m-1, n-1 ,0 , 0 , dp); 
+//         vector<vector<int>>dp(m,vector<int>(n,-1));
+//         for(int i=0 ; i<m;i++)
+//         {
+//             for(int j=0 ; j<n;j++)
+//             {
+//                 if(i==0 || j==0)
+//                 {
+//                     dp[i][j] = 1;
+//                 }
+//                 else
+//                 {
+//                     dp[i][j] = dp[i-1][j]+dp[i][j-1]; 
+//                 }
+                    
+//             }
+//         }
+//         return dp[m-1][n-1]; 
+        
+    }
+};
