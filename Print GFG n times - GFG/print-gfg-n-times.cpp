732d3296ac680625a98656857b14d5da276a1a3a
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-07-02 01:29:44 +0530

    Added solution - LeetHub

--- Print GFG n times - GFG/print-gfg-n-times.cpp
+++ Print GFG n times - GFG/print-gfg-n-times.cpp
@@ -7,8 +7,9 @@ using namespace std;
 class Solution {
   public:
     void printGfg(int N) {
-        if(N<=0) return ; 
-        cout<<"GFG"<<" "; 
+        if(N<=0)
+        return   ; 
+        cout<<"GFG " ; 
         printGfg(N-1) ; 
     }
 };
