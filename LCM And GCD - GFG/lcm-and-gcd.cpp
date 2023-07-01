4b88998432f45c91ac9c4a70ac15f22bd7e82253
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-07-02 01:28:08 +0530

    Added solution - LeetHub

--- LCM And GCD - GFG/lcm-and-gcd.cpp
+++ LCM And GCD - GFG/lcm-and-gcd.cpp
@@ -6,11 +6,7 @@ using namespace std;
 class Solution {
   public:
     vector<long long> lcmAndGcd(long long A , long long B) {
-      vector<long long> ans ; 
-        ans.push_back((A*B)/__gcd(A,B)) ; 
-      ans.push_back(__gcd(A,B)) ; 
-    
-      return ans ; 
+        return {(A*B)/__gcd(A,B) , __gcd(A,B)} ; 
     }
 };
 
