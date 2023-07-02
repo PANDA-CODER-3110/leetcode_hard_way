997c5984e634e6ffc8f3c436b0b1880ae9b05f91
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-07-02 22:16:09 +0530

    Time: 22 ms (50.82%), Space: 12.6 MB (5.56%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-       //Key is the number and value is its index in the vector.
-	unordered_map<int, int> hash;
-	vector<int> result;
-	for (int i = 0; i < numbers.size(); i++) {
-		int numberToFind = target - numbers[i];
-
-            //if numberToFind is found in map, return them
-		if (hash.find(numberToFind) != hash.end()) {
-                    //+1 because indices are NOT zero based
-			result.push_back(hash[numberToFind] );
-			result.push_back(i );			
-			return result;
-		}
-
-            //number was not found. Put it in the map.
-		hash[numbers[i]] = i;
-	}
-	return result;
-        
+    map<int,int>mp ; 
+        for(int i=0 ; i<numbers.size();i++)
+        {
+            mp[numbers[i]]= i ; 
+        }
+        for(int i=0 ;i<numbers.size() ;i++){
+            int rem = target - numbers[i] ;
+              if(mp.find(rem)!=mp.end() && i!=mp[rem]) 
+              {
+                  return { i , mp[rem]} ; 
+              }
+              
+        }
+        return {-1,-1} ; 
     }
 };
