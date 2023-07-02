59bccd5753a112d96679bf23124a684d5f62a735
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-07-02 20:18:31 +0530

    Added solution - LeetHub

--- Missing number - GFG/missing-number.cpp
+++ Missing number - GFG/missing-number.cpp
@@ -24,10 +24,11 @@ int main()
 
 
 int missingNumber(int A[], int N)
-{int sum =0 ; 
-   for(int i=0 ; i<N-1;i++)
-   {
-       sum +=A[i] ; 
-   }
-   return abs(sum - (N*(N+1))/2) ; 
+{
+    int sum =0 ; 
+    for(int i=0 ; i<N-1;i++)
+    {
+        sum+=A[i] ;
+    }
+    return (N*(N+1)/2) - sum ; 
 }
