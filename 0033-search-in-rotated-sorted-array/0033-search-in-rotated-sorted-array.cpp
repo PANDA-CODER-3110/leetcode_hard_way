d70802f1c12a5723ded02001b64128d4ce06c811
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-07-04 07:20:15 +0530

    Time: 3 ms (79.33%), Space: 11.1 MB (49.68%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-         int lo = 0 , hi = nums.size()-1; 
-         while(lo<=hi)
-         {  int m  =(lo+hi)/2;
-             if(nums[m]==target)
-             return m;
-           if(nums[lo]<=nums[m])  
-            {
-                if(nums[lo]<=target &&  nums[m]>=target)
-                {
-                    hi = m-1;
-                }
-                else
-                lo = m+1;
-            }
-            else{
-            if(nums[m]<=target && nums[hi]>=target)
-            {
-                lo = m+1;
-
-            }
-            else
-            {
-                hi = m-1;
-            }
-            }
-         }
-   
-        return -1;
+    int search(vector<int>&A, int target) {
+        int l = 0 , h = A.size()-1 ; 
+        int key= target ; 
+          while(l<=h)
+      {
+          int m = (l+h)/2; 
+          if(A[m]==key) return m ;  
+          else if(A[l]<=A[m])
+          {
+              if(A[l]<=key && key<=A[m])
+              {
+                  h = m-1 ; 
+              }
+              else
+              l= m+1  ; 
+          }
+          else
+          {
+              if(A[m]<=key && key<=A[h])
+              {
+                  l = m+1 ; 
+              }
+              else
+             h = m-1 ; 
+          }
+      }
+      return - 1; 
     }
 };
