c354cf21ccc9a9f18758e7455d5564b48945d793
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-07-04 20:57:48 +0530

    Time: 10 ms (28.35%), Space: 9.6 MB (89.23%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -4,7 +4,7 @@ public:
         int l =0 , r = arr.size()-1 ; 
         while(l<=r)
         {
-            int m = l+ (r-l)/2 ; 
+            int m = (r+l)/2 ; 
             if(arr[m] - m - 1 >= k)
                 r = m-1 ; 
             else 
