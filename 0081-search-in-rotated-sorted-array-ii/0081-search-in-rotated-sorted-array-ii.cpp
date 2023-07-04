02c5274683cddda4d0043e5eb0bba6c371007272
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-07-04 07:23:18 +0530

    Time: 8 ms (39.88%), Space: 13.9 MB (56.74%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool search(vector<int>&A, int Key) {
+         int l = 0 , h = A.size() -1 ; 
+        int key = Key ; 
+         while(l<=h)
+      {
+          int m = (l+h)/2; 
+          if(A[m]==key) return true ;  
+          if(A[l]==A[m] && A[h]==A[m])
+          {
+              l++ ; 
+              h-- ; 
+          }
+          else if(A[l]<=A[m])
+          {
+              if(A[l]<=key && key<=A[m])
+              {
+                  h = m-1 ; 
+              }
+              else
+              l= m+1  ; 
+          }
+          else
+          {
+              if(A[m]<=key && key<=A[h])
+              {
+                  l = m+1 ; 
+              }
+              else
+             h = m-1 ; 
+          }
+      }
+      return false ; 
+    }
+};
