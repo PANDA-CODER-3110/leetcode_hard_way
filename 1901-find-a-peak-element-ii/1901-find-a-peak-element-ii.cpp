4c619ca48e18c17e46e0b16e9d3120e1d6e843a9
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-07-06 05:28:35 +0530

    Time: 199 ms (50.64%), Space: 46.2 MB (81.20%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -2,26 +2,23 @@ class Solution {
 public:
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
         int row = mat.size() ; 
-        int col = mat[0].size() ; 
-        int i= 0  , j = col -1 ; 
-        while(i>=0  && i<row  && j>=0  && j<col)
+        int col = mat[0].size() ;
+         int i =0 , j = col -1 ; 
+        while(i>=0 && i<row && j>=0 && j<col)
         {
-            int up = i-1<0 ? -1 : mat[i-1][j] ; 
-            int down = i+1>=row ? -1 : mat[i+1][j] ; 
-            int l = j-1<0 ? -1 : mat[i][j-1] ; 
-            int r = j+1>=col ? -1 : mat[i][j+1] ;
-            
-            if(mat[i][j]>up && mat[i][j]>down && mat[i][j]>l && mat[i][j]>r)
+            int up = i-1<0? -1 : mat[i-1][j]  ; 
+             int down = i+1>=row? -1 : mat[i+1][j]  ; 
+             int left = j-1<0? -1 : mat[i][j-1]  ; 
+             int right = j+1>=col? -1 : mat[i][j+1]  ; 
+            if(mat[i][j]>up && mat[i][j]>down && mat[i][j]>left && mat[i][j]>right )
                 return {i,j} ; 
             
-            int maxx = max({up , down ,  l , r}) ; 
-            
-            if(maxx==up) i-- ; 
-            else if(maxx== down) i++  ;
-            else if(maxx==l) j-- ; 
+            int maxx = max({up, down , left , right}) ; 
+            if(maxx==up)i-- ; 
+            else if(maxx==down)i++ ; 
+            else if(maxx==left) j-- ; 
             else j++ ; 
         }
-        
         return {-1,-1}; 
     }
 };
