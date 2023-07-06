51ded547181d95433d65a534ae9bc7d3f4525924
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-07-06 08:04:22 +0530

    Added solution - LeetHub

--- Search in Linked List - GFG/search-in-linked-list.cpp
+++ Search in Linked List - GFG/search-in-linked-list.cpp
@@ -27,13 +27,15 @@ class Solution {
   public:
     // Function to count nodes of a linked list.
     bool searchKey(int n, struct Node* head, int key) {
-       while(head->next!=NULL)
-       {
-           if(head->data == key ) return true ; 
-           head = head->next ; 
-       }
-       
-       return false; 
+        Node* curr = head ; 
+        while(curr->next)
+        {
+            if(curr->data == key)
+            return true ; 
+            
+            curr= curr->next ; 
+        }
+        return false ;
     }
 };
 
