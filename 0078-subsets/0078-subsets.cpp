d43e1ce22bcca958352a4bd1b64d62e38107cdfc
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-07-08 23:55:37 +0530

    Time: 5 ms (37.20%), Space: 7.1 MB (76.17%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>>res ; 
-    void solve(int ind , vector<int>&ans , vector<int>& nums , int n )
+    void solve(vector<int>& nums , vector<int>&pre ,vector<vector<int>>&ans , int ind , int n )
     {
-        if(ind ==n)
+        if(ind==n)
         {
-            res.push_back(ans) ;
+           ans.push_back(pre) ; 
             return ; 
         }
-        ans.push_back(nums[ind]) ; 
-        solve(ind+1 ,ans , nums, n) ;
-         ans.pop_back() ; 
-        solve(ind+1 ,ans , nums, n) ;
+        pre.push_back(nums[ind] ) ; 
+        solve(nums , pre , ans , ind+1 , n) ; 
+          pre.pop_back( ) ; 
+        solve(nums , pre , ans , ind+1 , n) ;
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int>ans ; 
-        solve(0 , ans , nums ,nums.size()) ; 
-        return res ; 
+        vector<vector<int>>ans ; 
+        vector<int>pre ; 
+        solve(nums ,pre , ans , 0  , nums.size()) ; 
+        return ans ; 
     }
 };
