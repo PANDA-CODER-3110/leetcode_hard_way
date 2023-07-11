a6c731b11779db73c8c66d958c1f1c3e4c61f241
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-07-11 19:58:18 +0530

    Time: 23 ms (68.81%), Space: 7.8 MB (94.17%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-         vector<int> map(128,0);
-        for(auto c: t) map[c]++;
-        int counter=t.size(), begin=0, end=0, d=INT_MAX, head=0;
-        while(end<s.size()){
-            if(map[s[end++]]-->0) counter--; //in t
-            while(counter==0){ //valid
-                if(end-begin<d)  d=end-(head=begin);
-                if(map[s[begin++]]++==0) counter++;  //make it invalid
-            }  
+        if(s.size() < t.size()){
+            return "";
         }
-        return d==INT_MAX? "":s.substr(head, d);
+        unordered_map<char,int> map;
+        for(int i=0;i<t.size();i++){
+            map[t[i]]++;
+        }
+        int count=0,start=0,min_length = INT_MAX, min_start = 0;
+        for(int end=0; end<s.size(); end++){
+            if(map[s[end]]>0){
+                count++;
+            }
+            map[s[end]]--; 
+            if(count == t.length()) { 
+                while(start < end && map[s[start]] < 0){
+                    map[s[start]]++, start++;
+                } 
+                if(min_length > end-start){
+                    min_length = end-(min_start=start)+1; 
+                }
+                map[s[start++]]++; 
+                count--;
+            }
+        }
+        return min_length == INT_MAX ? "" : s.substr(min_start, min_length);
     }
 };
