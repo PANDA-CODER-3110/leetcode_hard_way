e89f6e1016b92fbaeb853d30a435ab432f8737e1
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-07-18 22:50:48 +0530

    Time: 7 ms (62.36%), Space: 12.6 MB (92.18%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -7,6 +7,13 @@
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ Approach: (As mentioned by Striver)
+
+At a node(say cur) if there exists a left child, we will find the rightmost node in the left subtree(say prev).
+We will set prev’s right child to cur’s right child,
+We will then set cur’s right child to it’s left child.
+We will then move cur to the next node by assigning cur it to its right child
+We will stop the execution when cur points to NULL.
  * };
  */
 class Solution {
