d1b3d231ab5675ccc6a385369294d59048035dba
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-07-19 11:05:11 +0530

    Time: 3 ms (71.24%), Space: 6 MB (50.35%) - LeetHub

--- 2745-construct-the-longest-new-string/2745-construct-the-longest-new-string.cpp
+++ 2745-construct-the-longest-new-string/2745-construct-the-longest-new-string.cpp
@@ -1,22 +1,6 @@
 class Solution {
 public:
     int longestString(int x, int y, int z) {
-          int AAStart = 0;
-        int BBStart = 0;
-        while(x>0 && y>0){
-            AAStart+=2;
-            BBStart+=2;
-            x--;
-            y--;
-        }
-        if(x>0){
-            AAStart++;
-        }
-        if(y>0){
-            BBStart++;
-        }
-        int ans = max(AAStart,BBStart);
-        ans += z;
-        return ans*2;
+          return 2*z+2*(x!=y?min(x,y)+1:x)+2*min(x,y);
     }
 };
