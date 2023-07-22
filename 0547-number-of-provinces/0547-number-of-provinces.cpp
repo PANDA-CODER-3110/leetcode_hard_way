fb9ba2f5b5fe234542624fb3c9ebd671aa60cba9
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-07-22 09:26:34 +0530

    Time: 28 ms (64.67%), Space: 15.3 MB (35.98%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
+    void dfs(vector<int>&vis , vector<vector<int>>& M , int node)
+    {
+        vis[node] =1 ; 
+        for(int i =0 ; i<M[node].size();i++)
+        {
+            if(vis[i]!=1 && M[node][i])
+                dfs(vis , M , i) ; 
+        }
+    }
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size(); 
-     if(n==0)  return 0 ; 
-        vector<bool>vis(n,false); 
-        int pro =0 ; 
+        int n = isConnected.size() ; 
+     vector<int>vis(n+1 , 0) ;
+         int cnt=0 ; 
         for(int i=0 ; i<n;i++)
         {
             if(!vis[i])
             {
-                pro++;
-                dfs(i,isConnected,vis);
+                 cnt++ ; 
+                dfs(vis , isConnected,i ) ; 
+                   
             }
         }
-        return pro ; 
-    }
-    void dfs(int i  , vector<vector<int>>&M, vector<bool>&v)
-    {
-        v[i] = true ; 
-        for(int j=0 ; j<M[i].size();j++)
-        {
-            if(M[i][j] && !v[j])
-            {
-                dfs(j,M,v);
-            }
-        }
+        return cnt ; 
     }
 };
