d28047b0dd92b0810463d9cf9438fc253eaa7fc5
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-07-24 23:38:22 +0530

    Time: 130 ms (78.97%), Space: 42.2 MB (76.73%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,31 +1,58 @@
-class Solution {
-private:
-    void dfs(vector<vector<int>> &adj, vector<bool> &visited, int src)
+class DisjointSet{
+       
+    public: 
+     vector<int>rank , parent , size ; 
+    DisjointSet(int n )
     {
-        visited[src] = true;
-        for(int i : adj[src])
-            if(!visited[i])
-                dfs(adj, visited, i);
+        rank.resize(n , 0) ; 
+        parent.resize(n , 0)  ; 
+        for(int i=0 ; i<n;i++){
+            parent[i] = i  ; 
+        }
     }
+    int find(int node)
+    {
+        if(node==parent[node])
+        return node ; 
+        return parent[node] = find(parent[node]) ;
+    }
+    void union_by_size(int u ,int v ){
+        int ul_u = find(u) ; 
+        int ul_v = find(v) ;
+        if(ul_u==ul_v)
+        return ; 
+        if(rank[ul_u]<rank[ul_v]){
+            parent[ul_u] = ul_v ;  
+        }
+        else if(rank[ul_u]>rank[ul_v])
+        parent[ul_v] = ul_u ; 
+        else
+        {
+            parent[ul_v] = ul_u ; 
+            rank[ul_u]++ ; 
+        }
+        
+    }
+};
+class Solution {
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
-        
-        if(connections.size() < n - 1)
-            return -1;
-        vector<vector<int>> adj(n);
-        for(auto v : connections)
-        {
-            adj[v[0]].push_back(v[1]);
-            adj[v[1]].push_back(v[0]);
+        DisjointSet ds(n) ; 
+       int cnt=0; 
+        for(auto it : connections){
+            if(ds.find(it[0])==ds.find(it[1]))
+                cnt++ ; 
+            else
+                ds.union_by_size(it[0] , it[1]) ; 
         }
-        vector<bool> visited(n, false);
-        int components = 0;
-        for(int i=0; i<n; i++)
-            if(!visited[i])
-            {
-                dfs(adj, visited, i);
-                components++;
-            }
-        return components - 1;
+        int ans =0 ; 
+        for(int i=0 ;i<n;i++){
+            if(ds.parent[i]==i)
+                ans++ ; 
+        }
+        
+      if(cnt>=ans-1)
+          return ans -1 ; 
+        return -1 ; 
     }
 };
