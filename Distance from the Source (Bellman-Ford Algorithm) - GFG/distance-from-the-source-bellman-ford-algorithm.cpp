98b06d9d77f6888088ba0ff074dda3a16513a838
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-07-24 14:55:57 +0530

    Added solution - LeetHub

--- Distance from the Source (Bellman-Ford Algorithm) - GFG/distance-from-the-source-bellman-ford-algorithm.cpp
+++ Distance from the Source (Bellman-Ford Algorithm) - GFG/distance-from-the-source-bellman-ford-algorithm.cpp
@@ -13,35 +13,25 @@ class Solution {
     *   V: number of vertices
     */
     vector<int> bellman_ford(int V, vector<vector<int>>& edges, int S) {
-        vector<int>dist(V,1e8); 
+        vector<int>dist(V , 1e8) ; 
         dist[S] =0 ; 
-        for(int i=0 ; i<V-1;i++)
+        for(int i =0 ; i<V-1;i++)
         {
             for(auto it : edges)
             {
-                int u = it[0]; 
-                int v = it[1]; 
-                int wt = it[2]; 
-                if(dist[u]!=1e8 && dist[u]+wt<dist[v])
-                {
-                    dist[v] = dist[u]+wt; 
+                if(dist[it[0]]!=1e8 && dist[it[0]]+it[2]<dist[it[1]]){
+                    dist[it[1]] = dist[it[0]]+it[2] ; 
                 }
             }
         }
-       for(int i=0 ; i<V-1;i++)
+        for(auto it : edges)
         {
-            for(auto it : edges)
-            {
-                int u = it[0]; 
-                int v = it[1]; 
-                int wt = it[2]; 
-                if(dist[u]!=1e8 && dist[u]+wt<dist[v])
-                {
-                    return {-1}; 
-                }
-            }
+            if(dist[it[0]]!=1e8 && dist[it[0]]+it[2]<dist[it[1]])
+            return {-1} ; 
         }
+        
         return dist ; 
+        
     }
 };
 
