6dc0e220b6b034b217306056052c64c0e33268d1
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-07-29 16:13:07 +0530

    Time: 1230 ms (69.48%), Space: 14.4 MB (76.57%) - LeetHub

---
+++ 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int n, m;
+    int target;
+    int dp[71][5000]; 
+    int solve(int row, int sum, vector<vector<int>>& mat){
+        if(row == n){
+            return abs(sum - target);
+        }
+        if(dp[row][sum] != -1) return dp[row][sum];
+        int ans = 1e9;
+        for(int j = 0; j < m; ++j){
+            ans = min(ans, solve(row+1, sum+mat[row][j], mat));
+        }
+        return dp[row][sum] = ans;
+    }
+    int minimizeTheDifference(vector<vector<int>>& mat, int t) {
+        n = mat.size();
+        m = mat[0].size();
+        target = t;
+        memset(dp, -1, sizeof(dp));
+        return solve(0, 0, mat);
+
+    }
+};
