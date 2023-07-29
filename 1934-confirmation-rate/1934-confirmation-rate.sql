b7b9a20c94019e09cf054fa6fbead7f21f7445d8
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-07-29 10:43:33 +0530

    Time: 1427 ms (72.58%), Space: 0B (100.00%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -3,4 +3,4 @@ select s.user_id  ,round(avg(if(c.action ='confirmed'  , 1, 0)),2) as confirmati
 from signups s left join confirmations c 
 on s.user_id = c.user_id
 group by s.user_id 
-order by  confirmation_rate desc   
+order by  confirmation_rate   
