71536eb6df1b74c3890bae1547738ce01c52deb3
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-07-30 18:33:42 +0530

    Time: 38 ms (99.71%), Space: 33.6 MB (61.85%) - LeetHub

---
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int>ans ; 
+        for(int i =0 ;i<nums.size();i++){
+            int pos = abs(nums[i]) -1 ; 
+            if(nums[pos]<0)ans.push_back(pos+1) ; 
+            nums[pos] = -nums[pos] ; 
+        }
+        return ans ; 
+    }
+};
