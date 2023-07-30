55d4df6b1a6c4e25b79498e5fbf04c981f573cf1
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-07-30 15:03:46 +0530

    Time: 18 ms (98.55%), Space: 22.3 MB (74.79%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,11 +1,37 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int ans =0  ; 
-        for(int i = 0 ; i<nums.size();i++)
-        {
-            ans^=nums[i] ; 
-        }
-        return ans ; 
+          int n = nums.size();
+            if(n == 1){
+                return nums[0];
+            }
+            //first element is single
+            if(nums[0] != nums[1]){
+                return nums[0];
+            }
+            //last element is single
+            if(nums[n-1] != nums[n-2]){
+                return nums[n-1];
+            }
+
+            int start = 1;
+            int end = n-2;
+
+            while(start <= end){
+                int mid = start + (end-start)/2;
+
+                if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
+                    return nums[mid];
+                }
+                //left half
+                if(mid%2==0 && nums[mid]==nums[mid+1] || mid%2==1 && nums[mid]==nums[mid-1]){
+                    start = mid + 1;
+                }
+                //right half
+                else{
+                    end = mid ;
+                }
+            }
+            return -1;
     }
 };
