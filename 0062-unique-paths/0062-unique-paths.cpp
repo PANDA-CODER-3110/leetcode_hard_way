9c5bc34f90da994d47690603e6ec7ee81653e426
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-08-01 11:33:14 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (69.16%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -16,25 +16,28 @@ public:
     }
     int uniquePaths(int m, int n) {
         
-          vector<vector<int>>dp(m,vector<int>(n,-1));
-        return f(0, 0 ,m-1 , n-1 , dp); 
-//         vector<vector<int>>dp(m,vector<int>(n,-1));
-//         for(int i=0 ; i<m;i++)
-//         {
-//             for(int j=0 ; j<n;j++)
-//             {
-//                 if(i==0 || j==0)
-//                 {
-//                     dp[i][j] = 1;
-//                 }
-//                 else
-//                 {
-//                     dp[i][j] = dp[i-1][j]+dp[i][j-1]; 
-//                 }
+          vector<int>dp(n,-1) , temp(n , 0);
+       // return f(0, 0 ,m-1 , n-1 , dp); 
+       // vector<vector<int>>dp(m,vector<int>(n,-1));
+        
+        for(int i=0 ; i<m;i++)
+        {
+            vector<int>temp(n ,0) ; 
+            for(int j=0 ; j<n;j++)
+            {
+                if(i==0 || j==0)
+                {
+                    temp[j] =1 ; 
+                }
+                else
+                {
+                    temp[j] = dp[j]+temp[j-1]; 
+                }
                     
-//             }
-//         }
-//         return dp[m-1][n-1]; 
+            }
+            dp = temp ; 
+        }
+        return dp[n-1]; 
         
     }
 };
