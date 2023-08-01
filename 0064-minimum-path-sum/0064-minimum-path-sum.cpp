781b3826bc94f4e79fc7e9c774ddad501c395c14
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-08-01 11:55:49 +0530

    Time: 12 ms (53.70%), Space: 10 MB (43.45%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int solve(int i,int j,vector<vector<int>>& grid,vector<vector<int>>&dp,int m,int n)
     {   
-	    if(i>=m || j>=n) return 1e9 ;// If index is out of bound then returning large value
-        if(i==m-1&& j==n-1)return grid[i][j];
+	    if(i<0 || j<0) return 1e9 ;// If index is out of bound then returning large value
+        if(i==0 && j== 0)return grid[i][j];
         if(dp[i][j]!=-1) return dp[i][j];
-        int down=grid[i][j]+solve(i+1,j,grid,dp,m,n);
-        int right=grid[i][j]+solve(i,j+1,grid,dp,m,n);
-        return dp[i][j]=min(down,right);
+        int up =grid[i][j]+solve(i-1,j,grid,dp,m,n);
+        int left =grid[i][j]+solve(i,j-1,grid,dp,m,n);
+        return dp[i][j]=min(left,up);
     }
     int minPathSum(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
         vector<vector<int>>dp(m+1,vector<int> (n+1,-1));
-        return solve(0,0,grid,dp,m,n);
+        return solve(m-1,n-1,grid,dp,m,n);
     }
 };
