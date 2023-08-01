3c3f64adb8eade00c0fa97ecdb9004a0ad6b2f56
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-08-01 21:27:53 +0530

    Time: 43 ms (44.21%), Space: 18.3 MB (64.71%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -20,8 +20,24 @@ public:
         return dp[ind][tar] = (take+ nottake) ; 
     }
     int change(int n, vector<int>&S) {
-    vector<vector<int>>dp(S.size() , vector<int>(n+1 ,-1)) ; 
-       return  solve(n , S , S.size()-1 , dp) ; 
+    vector<vector<int>>dp(S.size() , vector<int>(n+1 ,0)) ; 
+     //  return  solve(n , S , S.size()-1 , dp) ; 
+        for(int i =0 ; i<=n ; i++)
+        {
+            if(i%S[0]==0)
+                dp[0][i] = (i%S[0]==0) ; 
+        }
+        
+        for(int i =1 ;i <S.size() ; i++){
+            for(int j =0 ; j<=n ; j++){
+                int nottake = dp[i-1][j] ; 
+                int take = 0 ; 
+                if(S[i]<=j)
+                    take = dp[i][j-S[i]] ;
+                dp[i][j] = (take+ nottake) ; 
+            }
+        }
+        return dp[S.size()-1][n]  ; 
     }
      
 };
