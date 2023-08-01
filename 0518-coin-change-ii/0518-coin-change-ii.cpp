37da99c89fe352eb3e6cd0cf944223552e07364b
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-08-01 20:09:52 +0530

    Time: 6 ms (97.56%), Space: 18.5 MB (36.60%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-     int rec(int idx, int tar,  vector<int>& coins,vector<vector<int>> &dp)
+    int solve( int tar ,vector<int>&S, int ind  , vector<vector<int>>&dp)
     {
-        if(idx==0){
-            if(tar%coins[idx]==0)
+       if(ind==0){
+            if(tar%S[ind]==0)
             return 1;
 
             else
             return 0;
         }
-      if(dp[idx][tar]!=-1) return dp[idx][tar];
-        int ntk = rec(idx-1,tar,coins,dp);
-        int tk = 0;
-        if(coins[idx]<=tar){
-            tk = rec(idx,tar-coins[idx],coins,dp);
-        }
-
-        return dp[idx][tar]=tk+ntk;
+        if(dp[ind][tar]!=-1)
+           return dp[ind][tar] ; 
+        
+        int nottake = solve(tar , S , ind-1  ,dp) ; 
+        int take = 0 ; 
+        if(S[ind]<=tar)
+            take = solve(tar-S[ind] , S , ind , dp ) ; 
+        
+        return dp[ind][tar] = (take+ nottake) ; 
     }
-public:
-    int change(int amount, vector<int>& coins) {
-        vector<vector<int>> dp(coins.size(), vector<int>(amount+1,-1));
-        return rec(coins.size()-1,amount, coins,dp);
+    int change(int n, vector<int>&S) {
+    vector<vector<int>>dp(S.size() , vector<int>(n+1 ,-1)) ; 
+       return  solve(n , S , S.size()-1 , dp) ; 
     }
+     
 };
