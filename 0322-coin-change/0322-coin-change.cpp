b230fc027b62386b3c539d4aba019c158d23deea
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-08-01 19:41:40 +0530

    Time: 108 ms (65.32%), Space: 45 MB (39.99%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -14,9 +14,30 @@ public:
         if(c[ind]<=amt) take = solve(c , amt - c[ind] , ind , dp )+1 ; 
         return dp[ind][amt] = min(take , nottake ) ; 
     }
-    int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>>dp(coins.size(), vector<int>(amount+1, -1))   ; 
-        int ans = solve(coins , amount , coins.size()-1 , dp) ; 
-        return (ans ==1e9)?-1 : ans ; 
+    int coinChange(vector<int>& c, int amount) {
+        vector<vector<int>>dp(c.size(), vector<int>(amount+1,0))   ; 
+        // int ans = solve(coins , amount , coins.size()-1 , dp) ; 
+         for(int i=0; i<=amount; i++){
+        if(i%c[0] == 0)  
+            dp[0][i] = i/c[0];
+        else dp[0][i] = 1e9;
+    }
+        
+        for(int i =1 ;i<c.size();i++){
+            for(int j =0 ; j<=amount ; j++){
+                    int nottake = 0 +dp[i-1][j] ; 
+                    int take = 1e9 ; 
+                    if(c[i]<=j) 
+                        take = 1+dp[i][j - c[i]] ; 
+                
+                 dp[i][j] = min(take , nottake ) ; 
+                
+              
+            }
+        }
+        
+  int ans = dp[c.size()-1][amount];
+    if(ans >=1e9) return -1;
+    return ans;
     }
 };
