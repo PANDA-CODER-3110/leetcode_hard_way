a4601a9b0636c4b9bbabcf6473c1a701db75cc3e
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-08-01 11:52:50 +0530

    Time: 5 ms (25.74%), Space: 7.7 MB (80.87%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -14,28 +14,30 @@ public:
         
         int n = mat.size()  ;
         int m = mat[0].size() ;
-        vector<vector<int>>dp( n , vector<int>(m ,0)) ; 
+        vector<int>dp(m ,0) ; 
        // return solve(n-1 , m-1 , obstacleGrid , dp) ; 
         for(int i =0 ; i<n;i++){
+            vector<int>cur(m ,0) ; 
             for(int j =0 ; j<m;j++){
                 if(mat[i][j]==1)
-                    dp[i][j] = 0 ; 
+                    cur[j] = 0 ; 
                 else if(i==0 && j==0)
-                    dp[i][j] = 1 ; 
+                    cur[j] = 1 ; 
                 else
                 {
                     int up =0 ; 
                     int left  =0 ; 
                     if(i>0)
-                        up = dp[i-1][j] ; 
+                        up = dp[j] ; 
                     if(j>0)
-                        left = dp[i][j-1] ; 
+                        left = cur[j-1] ; 
                     
-                    dp[i][j] = up+left  ; 
+                    cur[j] = up+left  ; 
                 }
                 
             }
+            dp = cur ; 
         }
-        return dp[n-1][m-1] ;  
+        return dp[m-1] ;  
     }
 };
