8ddd1d9cf0d5d6324ebaab188ad1d28aad7f540b
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-08-01 11:50:56 +0530

    Time: 2 ms (70.87%), Space: 7.8 MB (24.28%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -10,11 +10,32 @@ public:
         return dp[i][j] = solve(i-1 , j ,mat , dp)+solve(i , j-1 , mat , dp ) ;  
         
     }
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+    int uniquePathsWithObstacles(vector<vector<int>>& mat) {
         
-        int n = obstacleGrid.size()  ;
-        int m = obstacleGrid[0].size() ;
-        vector<vector<int>>dp( n , vector<int>(m , -1)) ; 
-       return solve(n-1 , m-1 , obstacleGrid , dp) ; 
+        int n = mat.size()  ;
+        int m = mat[0].size() ;
+        vector<vector<int>>dp( n , vector<int>(m ,0)) ; 
+       // return solve(n-1 , m-1 , obstacleGrid , dp) ; 
+        for(int i =0 ; i<n;i++){
+            for(int j =0 ; j<m;j++){
+                if(mat[i][j]==1)
+                    dp[i][j] = 0 ; 
+                else if(i==0 && j==0)
+                    dp[i][j] = 1 ; 
+                else
+                {
+                    int up =0 ; 
+                    int left  =0 ; 
+                    if(i>0)
+                        up = dp[i-1][j] ; 
+                    if(j>0)
+                        left = dp[i][j-1] ; 
+                    
+                    dp[i][j] = up+left  ; 
+                }
+                
+            }
+        }
+        return dp[n-1][m-1] ;  
     }
 };
