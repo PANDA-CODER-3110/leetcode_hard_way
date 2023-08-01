ba0cd3448d91e7b996cb9cf26bb811bc5670ac79
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-08-01 11:26:03 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (19.26%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -3,21 +3,21 @@ public:
     
      int f (int a , int b , int i , int j ,vector<vector<int>>&dp )
     {
-        if(i>=a+1 ||  j>=b+1)
+        if(i<0 ||  j<0)
         return  0 ; 
         if(i==a && j==b) return 1 ; 
         
         if(dp[i][j]!=-1)
         return dp[i][j] ;
          
-         return dp[i][j]  = f(a,b,i+1,j,dp)+f(a,b,i,j+1,dp) ; 
+         return dp[i][j]  = f(a,b,i-1,j,dp)+f(a,b,i,j-1,dp) ; 
          
         
     }
     int uniquePaths(int m, int n) {
         
           vector<vector<int>>dp(m,vector<int>(n,-1));
-        return f(m-1, n-1 ,0 , 0 , dp); 
+        return f(0, 0 ,m-1 , n-1 , dp); 
 //         vector<vector<int>>dp(m,vector<int>(n,-1));
 //         for(int i=0 ; i<m;i++)
 //         {
