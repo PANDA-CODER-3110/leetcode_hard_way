6815c47c9d96f4089221e3be01b12ad6d69437ae
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-08-03 08:48:26 +0530

    Time: 0 ms (100.00%), Space: 9.7 MB (46.73%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-int lo = 0, hi = nums.size(); // we might need to inseart at the end
-    while(lo < hi) { // breaks if lo == hi
-        int mid = lo + floor((hi-lo)/2); // always gives the lower mid
-        if (target > nums[mid]) {
-            lo = mid + 1  ; // no way mid is a valid option
-        } else {
-            hi = mid  ;// it might be possibe to inseart @ mid
-        }
-    }
-    return hi;
+     return lower_bound(nums.begin() , nums.end()  , target) - nums.begin() ;    
     }
 };
