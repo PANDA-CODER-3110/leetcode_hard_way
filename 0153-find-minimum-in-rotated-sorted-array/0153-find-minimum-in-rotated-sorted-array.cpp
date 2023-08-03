1bb4a068b5f44ee7d133f37895bc87ba0cf414f6
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-08-03 10:19:58 +0530

    Time: 0 ms (100.00%), Space: 10.2 MB (6.41%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,7 +1,21 @@
 class Solution {
 public:
     int findMin(vector<int>& arr) {
-       auto ans = *min_element(arr.begin() , arr.end()) ;
-       return ans ; 
+      int start =0 , end = arr.size() -1 ; 
+        int ans = INT_MAX  ; 
+        while(start <= end){
+            int mid = (start+end)/2 ; 
+            if(arr[start]<=arr[mid]){
+                ans = min(ans , arr[start]) ;
+                start = mid+1 ; 
+            }
+            else
+            {
+                ans = min(ans , arr[mid]) ; 
+                end = mid-1 ; 
+            }
+            
+        }
+        return ans ; 
     }
 };
