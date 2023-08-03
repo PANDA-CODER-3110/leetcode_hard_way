2e2412f29b49f646931ea1e6ad67cfc416a04eb8
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-08-03 18:10:00 +0530

    Time: 236 ms (75.68%), Space: 10.6 MB (51.38%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -14,8 +14,19 @@ public:
         return dp[ind][prev+1] =  len  ; 
     }
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>>dp(nums.size() , vector<int>(nums.size() , -1)) ; 
-    return solve(nums  , 0 , -1 , dp) ; 
-            
+      //  vector<vector<int>>dp(nums.size()+1 , vector<int>(nums.size()+1 , 0)) ; 
+   // return solve(nums  , 0 , -1 , dp) ; 
+        int maxi =0 ; 
+            vector<int>dp(nums.size(), 1) ;
+        int n = nums.size()  ; 
+        for(int i =0 ; i<n;i++){
+            for(int j = 0 ; j<i ; j++){
+                if(nums[i]>nums[j])
+                    dp[i] = max(dp[i] , 1+dp[j]) ; 
+            }
+            maxi = max(maxi , dp[i]) ; 
+        }
+        
+        return maxi  ; 
     }
 };
