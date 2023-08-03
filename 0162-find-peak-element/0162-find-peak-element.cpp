373f3cee88538f160e150a68025af73695cc9341
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-08-03 10:40:43 +0530

    Time: 6 ms (32.94%), Space: 8.9 MB (49.19%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& arr) {
-        int i=0,j=arr.size()-1;
-		while(i<j){
-			int mid=i+(j-i)/2;
-			if(arr[mid]>=arr[mid+1]){j=mid;}
-			else{i=mid+1;}
-		}
-		return i;
+       return max_element(arr.begin() , arr.end())-arr.begin() ; 
     }
 };
