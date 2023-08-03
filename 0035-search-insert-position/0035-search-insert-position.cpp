88d57777ff7e80b0d90ff6c34c75e70cf5f2bc83
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-08-03 08:53:11 +0530

    Time: 3 ms (87.04%), Space: 9.6 MB (46.73%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-     return lower_bound(nums.begin() , nums.end()  , target) - nums.begin() ;    
-    }
+     int start =0 , end = nums.size()-1  , ans = nums.size() ; 
+        while(start<=end){
+            int mid = (start + end)/2 ; 
+            if(nums[mid]>=target)
+            {
+                ans = mid ; 
+                end = mid-1  ; 
+            }
+            else
+                start = mid+1 ; 
+        }
+        
+        return ans ; 
+    } 
 };
