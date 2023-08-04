ef06d19777313cd4ba30b45c71f0def403eadbda
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-08-04 11:13:26 +0530

    Time: 1548 ms (57.22%), Space: 6.6 MB (57.41%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -26,7 +26,18 @@ public:
     } 
     int minCut(string s) {
         int n = s.size() ; 
-        vector<int>dp(n+1 ,-1) ;
-        return solve(0 , n , s ,dp ) -1 ; 
+        vector<int>dp(n+1 ,0) ;
+        //return solve(0 , n , s ,dp ) -1 ; 
+        for(int i =n-1 ;i>=0; i--){
+            int mini = INT_MAX ; 
+                for(int j =i ; j<n;j++){
+                    if(ispossible(i , j , s)){
+                        int cost = 1 +dp[j+1] ;
+                        mini = min(mini , cost ); 
+                    }
+                }
+            dp[i] =  mini ; 
+        }
+    return dp[0] -1; 
     }
 };
