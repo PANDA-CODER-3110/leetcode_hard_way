228ba476b4336ae6669f65521e9fdeb63fdfe183
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-08-04 11:35:40 +0530

    Time: 13 ms (89.23%), Space: 8.6 MB (14.17%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -22,8 +22,25 @@ public:
     }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
           int n = arr.size() ; 
-        vector<int>dp(arr.size()+1 , -1)  ; 
-        return solve(0 , arr , k ,dp) ; 
+        vector<int>dp(arr.size()+1 , 0)  ; 
+       // return solve(0 , arr , k ,dp) ; 
+        for(int i = n-1 ;i>=0 ; i-- ){
+             int len =0 ;
+            int maxi =INT_MIN ; 
+            int maxans = INT_MIN ; 
+            int n = arr.size() ; 
+            int lim = min(n,i+k) ; 
+            for(int j =i ; j< lim ; j++)
+            {
+                len++ ; 
+                maxi = max(maxi , arr[j]) ; 
+                int sum = (len*maxi) + dp[j+1] ; 
+                maxans= max(maxans , sum) ; 
+            }
+
+        dp[i] = maxans   ;
+        }
         
+        return dp[0] ; 
     }
 };
