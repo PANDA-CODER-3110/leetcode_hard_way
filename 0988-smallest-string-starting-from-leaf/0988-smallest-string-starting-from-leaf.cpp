400796352b986aa71ad259ae5a25618dc956b724
PANDA-CODER-3110 <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-08-05 16:08:42 +0530

    Time: 7 ms (91.46%), Space: 19.6 MB (57.54%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.cpp
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.cpp
@@ -11,29 +11,25 @@
  */
 class Solution {
 public:
-    vector<string>ans ; 
+   string ans = "~" ; 
     void solve(TreeNode* root , string s){
         if(root==NULL)
             return ; 
-        if(root->left || root->right)
-        s+=('a'+root->val) ; 
-        else
-        {
-             s+=('a'+root->val) ; 
-            ans.push_back(s)  ; 
-        }
+       if(root->left==NULL && root->right==NULL)
+       {
+           s = char('a'+root->val)+s ; 
+           ans= min(ans , s) ; 
+           return ; 
+       }   
         
-        solve(root->left ,s ) ; 
-        solve(root->right, s) ; 
+        solve(root->left ,char('a'+root->val)+s ) ; 
+        solve(root->right, char('a'+root->val)+s) ; 
         
     }
     string smallestFromLeaf(TreeNode* root) {
         solve(root , "") ; 
         
-        for(int i =0 ; i<ans.size();i++){
-            reverse(ans[i].begin() , ans[i].end()) ; 
-        } 
-        sort(ans.begin(), ans.end()) ; 
-        return ans[0] ; 
+       
+        return ans ; 
     }
 };
