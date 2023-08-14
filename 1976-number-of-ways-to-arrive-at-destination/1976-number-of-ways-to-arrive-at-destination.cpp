e79f040c522b0bf1946f802cb21b6b205e33f2d0
ADITYA SINGH <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-08-14 11:22:28 +0530

    Time: 74 ms (80.24%), Space: 32.6 MB (68.35%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -8,22 +8,24 @@ public:
             adj[u].push_back({v,w});
             adj[v].push_back({u,w});
         }
-        priority_queue<pair<long long,int>, vector<pair<long long,int>>, greater<pair<long long,int>>> pq;
+        set<pair<long long , int>>st ; 
         vector<long long> dist(n,1e15);
         vector<int> ways(n,0);
         dist[0] = 0;
         ways[0] = 1;
-        pq.push({0,0});
-        while(!pq.empty()){
-            long long distance = pq.top().first;
-            int node = pq.top().second;
-            pq.pop();
+        st.insert({0 ,0}) ; 
+        while(st.size()){
+            auto pl = (*st.begin()) ; 
+            long long distance = pl.first;
+            int node = pl.second;
+            st.erase(pl) ; 
             for(auto it: adj[node]){
                 int itNode = it.first;
                 long long itdistance = it.second;
                 if(dist[itNode]> itdistance+distance){
+                    st.erase({dist[itNode] , itNode}) ; 
                     dist[itNode] = itdistance + distance;
-                    pq.push({dist[itNode],itNode});
+                    st.insert({dist[itNode] , itNode}) ; 
                     ways[itNode] = ways[node];
                 }
                 else if(dist[itNode]==itdistance + distance){
