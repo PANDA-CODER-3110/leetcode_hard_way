f700849065d2960777ce05de35c520fb0f75c558
ADITYA SINGH <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-08-14 13:10:45 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,74 +3,40 @@
 using namespace std;
 
 // } Driver Code Ends
-
-class DisjointSet{
-    vector<int>rank , parent , size ; 
-    public: 
-    DisjointSet(int n )
-    {
-        rank.resize(n+1 , 0) ; 
-        parent.resize(n+1 , 0) , size.resize(n+1 , 0) ; 
-        for(int i=0 ; i<=n;i++){
-            parent[i] = i  ; 
-            size[i] =i ; 
-        }
-    }
-    int find(int node)
-    {
-        if(node==parent[node])
-        return node ; 
-        return parent[node] = find(parent[node]) ;
-    }
-    void union_by_size(int u ,int v ){
-        int ul_u = find(u) ; 
-        int ul_v = find(v) ;
-        if(ul_u==ul_v)
-        return ; 
-        if(rank[ul_u]<rank[ul_v]){
-            parent[ul_u] = ul_v ;  
-        }
-        else if(rank[ul_u]>rank[ul_v])
-        parent[ul_v] = ul_u ; 
-        else
-        {
-            parent[ul_v] = ul_u ; 
-            rank[ul_u]++ ; 
-        }
-        
-    }
-};
 class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[])
     {
-        vector<pair<int , pair<int, int>>>edges ; 
-        for(int i =0 ; i<V;i++){
-            for(auto it :adj[i])
-            {
-                edges.push_back({it[1],{i , it[0]}}) ; 
-            }
-        }
-        DisjointSet ds(V) ; 
-        sort(edges.begin() , edges.end()) ; 
-        int mstwt ; 
-        for(auto it : edges){
-            int wt = it.first ; 
-            int u = it.second . first ; 
-            int v = it.second . second ; 
-            if(ds.find(u)!=ds.find(v)){
-                mstwt+=wt ; 
-                ds.union_by_size(u,v) ; 
-            }
-        }
-        return mstwt ; 
-        
+        priority_queue<pair<int , int> , vector<pair<int , int>>,greater<pair<int , int>>>pq ;
+        vector<int>vis(V+1 , 0) ; 
+        pq.push({0,0}) ;
+         int ans =0 ; 
+         while(!pq.empty())
+         {
+             int dis = pq.top().first ; 
+             int node = pq.top().second ; 
+             pq.pop() ;
+              if(vis[node])
+              continue ; 
+              ans+=dis ; 
+              vis[node] =1 ; 
+              for(auto it : adj[node])
+              {
+                  int adjnode = it[0] ; 
+                  int dist = it[1] ; 
+                  if(!vis[adjnode])
+                  {
+                      pq.push({dist , adjnode}) ; 
+                  }
+              }
+         }
+         
+         return ans ; 
         
     }
 };
-
 
 //{ Driver Code Starts.
 
