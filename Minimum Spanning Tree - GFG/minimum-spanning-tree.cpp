c9083e10e1b59331e3edeedb0d6ab006d29acd37
ADITYA SINGH <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-08-15 11:41:28 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,39 +3,104 @@
 using namespace std;
 
 // } Driver Code Ends
+
 class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[])
     {
-        priority_queue<pair<int , int> , vector<pair<int , int>>,greater<pair<int , int>>>pq ;
-        vector<int>vis(V+1 , 0) ; 
-        pq.push({0,0}) ;
-         int ans =0 ; 
-         while(!pq.empty())
-         {
-             int dis = pq.top().first ; 
-             int node = pq.top().second ; 
-             pq.pop() ;
-              if(vis[node])
-              continue ; 
-              ans+=dis ; 
-              vis[node] =1 ; 
-              for(auto it : adj[node])
-              {
-                  int adjnode = it[0] ; 
-                  int dist = it[1] ; 
-                  if(!vis[adjnode])
-                  {
-                      pq.push({dist , adjnode}) ; 
-                  }
-              }
-         }
-         
-         return ans ; 
-        
+       vector<pair<int , pair<int, int >>>p ; 
+       for(int i =0 ; i<V ; i++)
+       {
+           for(auto it : adj[i])
+           {
+               p.push_back({it[1] , {it[0] , i}}) ;  
+           }
+       }
+       
+       Disjoint_set ds(V) ; 
+       sort(p.begin() , p.end()) ;
+       int ans ; 
+       for(auto it : p)
+       {
+           int wt = it.first ; 
+           int u = it.second.first ; 
+           int v = it.second.second ; 
+           if(ds.findupar(u)!=ds.findupar(v))
+           {
+               ans+=wt  ; 
+               
+               ds.unionbysize(u , v) ; 
+           }
+       }
+       
+       return ans  ; 
+       
     }
+class Disjoint_set
+{
+    vector<int> parent, size, rank;
+
+public:
+    Disjoint_set(int n)
+    {
+        rank.resize(n + 1, 0);
+        size.resize(n + 1, 0);
+        parent.resize(n + 1, 0);
+        for (int i = 0; i <= n; i++)
+        {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findupar(int node)
+    {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findupar(parent[node]);
+    }
+    void unionbyrank(int u, int v)
+    {
+        int ulp_u = findupar(u);
+        int ulp_v = findupar(v);
+        if (ulp_u == ulp_v)
+        {
+            return;
+        }
+        if (rank[ulp_u] < rank[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_u] > rank[ulp_v])
+        {
+            parent[ulp_v] = ulp_u;
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+    void unionbysize(int u, int v)
+    {
+        int ulp_u = findupar(u);
+        int ulp_v = findupar(v);
+        if (ulp_u == ulp_v)
+            return;
+        if (size[ulp_u] < size[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
 };
 
 //{ Driver Code Starts.
