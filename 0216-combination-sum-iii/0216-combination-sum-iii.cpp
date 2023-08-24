a5c38d0241dbef32e36ef082736c7cf633de2d0f
ADITYA SINGH <86154536+PANDA-CODER-3110@users.noreply.github.com>
2023-08-24 17:33:58 +0530

    Time: 3 ms (42.86%), Space: 6.5 MB (54.32%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,40 +1,35 @@
 class Solution {
-    
-    vector<vector<int>> ansArr;
-    
 public:
-    
-    int sum(vector<int>& combination){
-        int ans = 0;
-        for(auto x: combination){
-            ans += x;
-        }
-        return ans;
-    }
-    
-    void combinationSumHelper(int k, int n, vector<int>& combination, int num){
-        
-        if(k == 0){
-            if(sum(combination) == n ){
-                ansArr.push_back(combination);
-            }
+     vector<vector<int>>ans;
+     void combi(vector<int>& c, int idx, int t,vector<int> &v , int k  ){
+        if(t==0 && v.size()==k){
+            ans.push_back(v);
             return;
         }
+        if(idx==c.size()) return;
+
+        if(c[idx]<=t){
+          //we can pick
+            v.push_back(c[idx]);
+            combi(c,idx+1, t-c[idx],v ,  k );
+            v.pop_back();  
+        }
         
-        if(num > 9) return;
-        
-        combination.push_back(num);  // Including num in combination
-        combinationSumHelper(k - 1, n, combination, num + 1);
-        combination.pop_back(); // Not including num in combination
-        combinationSumHelper(k, n, combination, num + 1);    
-        
+        int j=idx+1;
+        while(j<c.size() and c[j]==c[j-1] ) j++;
+        //we dont pick
+        combi(c,j,t,v , k );
+
     }
     
     vector<vector<int>> combinationSum3(int k, int n) {
         
-        vector<int> combination;
-        combinationSumHelper(k, n, combination, 1);
-        return ansArr;
-        
+         vector<int>A;
+        for(int i=1;i<10;i++){
+            A.push_back(i);
+        }
+         vector<int>temp;
+            combi(A,0,n,temp , k  );
+        return ans ;
     }
 };
