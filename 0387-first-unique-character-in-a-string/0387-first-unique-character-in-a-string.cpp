64db202419a9c866b805ba8db86f0a2628bbd216
ADITYA SINGH <86154536+PANDA-CODER-3110@users.noreply.github.com>
2024-02-05 19:38:31 +0530

    Time: 15 ms (97.21%), Space: 11.9 MB (27.34%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        map<char,int>mp ; 
+       vector<int> mp(26,0);
         for(auto it : s)
+        mp[it-'a']++;
+
+        for(int i = 0; i < s.size(); i++)
         {
-            mp[it]++ ; 
+            if(mp[s[i]-'a'] == 1)
+            return i;
         }
-        
-   for(int i =0 ; i<s.size() ; i++)
-   {
-       if(mp[s[i]]==1)
-           return i ; 
-   }
-        return -1 ; 
+
+        return -1;
         
     }
 };
