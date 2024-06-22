0ec16b80d1a83eef3caacbb94b2a58fe2441840d
ADITYA SINGH <86154536+PANDA-CODER-3110@users.noreply.github.com>
2024-06-22 22:29:43 +0530

    Time: 88 ms (83.56%), Space: 73.2 MB (27.58%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -16,7 +16,19 @@ public:
         }
         return res;
     }
-    int numberOfSubarrays(vector<int>& A, int S) {
-        return atMost(A, S) - atMost(A, S - 1);
+    int numberOfSubarrays(vector<int>&nums, int k) {
+        // return atMost(A, S) ;
+        int n = nums.size();
+        vector<int> count(n+1, 0);
+        count[0] = 1;
+        int ans = 0, t = 0;
+        for(int v : nums){
+            t += v&1;
+            if(t-k >= 0){
+                ans += count[t-k];
+            }
+            count[t]++;
+        }
+        return ans;
     } 
 };
